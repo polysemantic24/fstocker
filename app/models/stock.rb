ec3f6d33bb86a4b1ae4acbba02340870d3c3e66e
polysemantic24 <pavel@arttaganka.ru>
2020-05-15 17:37:08 +0300

    iex api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,6 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:access_key_id], secret_token: Rails.application.credentials.iex_client[:secret_access_key], endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+  end
 end
